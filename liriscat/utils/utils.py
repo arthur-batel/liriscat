20c01d2825af3201cd708cb928f516e134f313ee
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-03-27 16:48:21 +0100

    i_fold correction

--- liriscat/utils/utils.py
+++ liriscat/utils/utils.py
@@ -111,7 +111,7 @@ def _generate_config(dataset_name: str = None, seed: int = 0, load_params: bool 
         'low_mem': low_mem,
         'n_query': n_query,
         'CDM': CDM,
-        'ifold': i_fold,
+        'i_fold': i_fold,
     }
 
 def generate_hs_config(dataset_name: str = None, seed: int = 0, load_params: bool = False,
