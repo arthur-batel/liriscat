4b8297c3ebbf55609cf77cb629152008d11dd125
Arthur Batel <arthur.batel@insa-lyon.fr>
2025-05-20 19:34:41 +0200

    test with loaded dataset objects

--- experiments/notebook_example/test.py
+++ experiments/notebook_example/test.py
@@ -1,21 +1,26 @@
 import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+os.environ["PYTHONHASHSEED"] = "0"
+os.environ['CUBLAS_WORKSPACE_CONFIG'] = ":4096:8"
 
-from liriscat import utils
-utils.set_seed(0)
-from liriscat import dataset
-from liriscat import selectionStrategy
-from liriscat import CDM
+import liriscat
+liriscat.utils.set_seed(0)
 
 import logging
 import gc
 import json
 import torch
+liriscat.utils.set_seed(0)
 import pandas as pd
 from importlib import reload
 import IMPACT
+import warnings
+import os
+import pickle
+os.chdir("./experiments/notebook_example/")
+print(os.getcwd())
 
-utils.setuplogger(verbose = True, log_name="liriscat")
+liriscat.utils.setuplogger(verbose = True, log_name="liriscat")
 
 import warnings
 import numpy as np
@@ -23,8 +28,9 @@ import numpy as np
 gc.collect()
 torch.cuda.empty_cache()
 
-config = utils.generate_eval_config(esc = 'error', valid_metric= 'mi_acc', pred_metrics = ["mi_acc"], profile_metrics = ['doa'], save_params=False, n_query=4, num_epochs=4, batch_size=512)
-utils.set_seed(config["seed"])
+
+config = liriscat.utils.generate_eval_config(load_params=True, esc = 'error', valid_metric= 'mi_acc', pred_metrics = ["mi_acc"], profile_metrics = ['doa'], save_params=False, n_query=4, num_epochs=1, batch_size=512)
+liriscat.utils.set_seed(config["seed"])
 
 config["dataset_name"] = "math2"
 logging.info(config["dataset_name"])
@@ -49,35 +55,34 @@ torch.cuda.empty_cache()
 warnings.filterwarnings("ignore", message="invalid value encountered in divide")
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-## Concept map format : {question_id : [category_id1, category_id2, ...]}
-concept_map = json.load(open(f'../datasets/2-preprocessed_data/{config["dataset_name"]}_concept_map.json', 'r'))
-concept_map = {int(k): [int(x) for x in v] for k, v in concept_map.items()}
-
 ## Metadata map format : {"num_user_id": ..., "num_item_id": ..., "num_dimension_id": ...}
 metadata = json.load(open(f'../datasets/2-preprocessed_data/{config["dataset_name"]}_metadata.json', 'r'))
 
 ## Tensor containing the nb of modalities per question
 nb_modalities = torch.load(f'../datasets/2-preprocessed_data/{config["dataset_name"]}_nb_modalities.pkl',weights_only=True)
 
-i_fold = 0
-## Dataframe columns : (user_id, question_id, response, category_id)
-train_df = pd.read_csv(
-    f'../datasets/2-preprocessed_data/{config["dataset_name"]}_train_{i_fold}.csv',
-    encoding='utf-8', dtype={'student_id': int, 'item_id': int, "correct": float,
-                                                             "dimension_id": int})
-valid_df = pd.read_csv(
-    f'../datasets/2-preprocessed_data/{config["dataset_name"]}_valid_{i_fold}.csv',
-    encoding='utf-8', dtype={'student_id': int, 'item_id': int, "correct": float,
-                                                             "dimension_id": int})
-test_df = pd.read_csv(
-    f'../datasets/2-preprocessed_data/{config["dataset_name"]}_test_{i_fold}.csv',
-    encoding='utf-8', dtype={'student_id': int, 'item_id': int, "correct": float,
-                                                             "dimension_id": int})
+config['inner_user_lr'] = 0.01
+config['num_inner_users_epochs'] = 8
+for i_fold in range(1) : 
+    ## Dataframe columns : (user_id, question_id, response, category_id)
+    
+    # Load the pickled datasets back into memory
+    train_path = f'../datasets/2-preprocessed_data/{config["dataset_name"]}_dataset_train_{i_fold}.pkl'
+    valid_path = f'../datasets/2-preprocessed_data/{config["dataset_name"]}_dataset_valid_{i_fold}.pkl'
+    test_path  = f'../datasets/2-preprocessed_data/{config["dataset_name"]}_dataset_test_{i_fold}.pkl'
 
-train_data = dataset.CATDataset(train_df, concept_map, metadata, config,nb_modalities)
-valid_data = dataset.EvalDataset(valid_df, concept_map, metadata, config,nb_modalities)
-test_data = dataset.EvalDataset(test_df, concept_map, metadata, config,nb_modalities)
+    with open(train_path, "rb") as f:
+        train_data = pickle.load(f)
+    with open(valid_path, "rb") as f:
+        valid_data = pickle.load(f)
+    with open(test_path, "rb") as f:
+        test_data = pickle.load(f)
 
-S = selectionStrategy.Random(metadata,**config)
+    train_data.load_config(config)
+    valid_data.load_config(config)
+    test_data.load_config(config)
 
-S.train(train_data, valid_data)
+    S = liriscat.selectionStrategy.Random(metadata,**config)
+    #S.train(train_data, valid_data)
+    S.init_models(train_data, valid_data)
+    print(S.evaluate_test(test_data))
