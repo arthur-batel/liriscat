b884ae0b7de92f5b67a38abe1397af0926404159
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-05-20 15:13:07 +0200

    Initialize train and valid users

--- liriscat/CDM/IMPACT.py
+++ liriscat/CDM/IMPACT.py
@@ -33,8 +33,13 @@ class CATIMPACT(IMPACT) :
 
     def __init__(self, **config):
         super().__init__(**config)
+        self.initialized_users_prior = False
+        
     def init_model(self, train_data: dataset.Dataset, valid_data: dataset.Dataset):
         super().init_model(train_data,valid_data)
+
+        # Replacement of pretrained users embeddings with randomly generated ones
+        self.init_users_prior(train_data, valid_data)
 
     def get_regularizer_with_pior(self):
         A = (self.model.users_emb.weight - self.model.prior_mean) 
@@ -46,36 +51,37 @@ class CATIMPACT(IMPACT) :
     def get_params(self):
         return self.model.state_dict()
 
-
-
     def init_test(self, test_data):
         self.model.R = test_data.log_tensor
         self.model.ir_idx = resp_to_mod(self.model.R, self.model.nb_modalities)
-        self.model.ir_idx = self.model.ir_idx.to(self.config['device'], non_blocking=True)
-        self.init_users_prior(test_data)
+        self.model.ir_idx = self.model.ir_idx.to(self.config['device'], non_blocking=True)    
 
-    def init_users_prior(self, test_data):
-        train_valid_users = torch.tensor(list(set(range(test_data.n_users)) - test_data.users_id),
-                                         device=self.config['device'])
+    def init_users_prior(self, train_data, valid_data):
+        """
+        Initialize users embedding and set the regularization term based on the posterior distribution learned on train and valid users.
+        """
+        train_valid_users = torch.tensor(list(train_data.users_id.union(valid_data.users_id)),device=self.config['device'])
         train_valid_users = train_valid_users.to(dtype=torch.long)
+        test_users = list(set(torch.arange(train_data.n_users).tolist()) - train_data.users_id - valid_data.users_id)
+        
+        # NO data leak to test dataset because we only look at the train and valid users 
         user_embeddings = self.model.users_emb(train_valid_users).float()
         
         ave = user_embeddings.mean(dim=0)
         std = user_embeddings.std(dim=0)
 
-        E = torch.normal(ave.expand(test_data.n_actual_users, -1), std.expand(test_data.n_actual_users, -1)/2)
+        E = torch.normal(ave.expand(train_data.n_users, -1), std.expand(train_data.n_users, -1)/2)
         E = E - E.mean(dim=0) + ave
-
-        E = E.to(self.config['device']).requires_grad_()
-        self.model.users_emb.weight.data[list(test_data.users_id), :] = E
+        with torch.no_grad():
+            self.model.users_emb.weight.copy_(E.to(self.config['device']))
 
         cov_matrix = torch.cov(user_embeddings.T).to(dtype=torch.float)
 
         self.model.prior_cov_inv = torch.inverse(cov_matrix)
-
         self.model.prior_mean = ave.unsqueeze(0)
         self.model.get_regularizer = functools.partial(self.get_regularizer_with_pior)
-
+        
+        self.initialized_users_prior = True
 
     def _compute_loss(self, users_id, items_id, concepts_id, labels):
         device = self.config['device']
@@ -98,10 +104,10 @@ class CATIMPACT(IMPACT) :
         R = self.model.get_regularizer()
 
         # Stack losses into a tensor
-        losses = torch.stack([L1, L3])  # Shape: (4,)
+        #losses = torch.stack([L1, L3])  # Shape: (4,)
 
         # Update statistics and compute weights
-        weights = self.L_W.compute_weights(losses)
+        #weights = self.L_W.compute_weights(losses)
 
         # Compute total loss
         total_loss = 1*L1 + lambda_param * R
--- liriscat/selectionStrategy/abstract_selection_strategy.py
+++ liriscat/selectionStrategy/abstract_selection_strategy.py
@@ -166,16 +166,15 @@ class AbstractSelectionStrategy(ABC):
                     category_ids = batch["category_ids"]
     
                     self.CDM.model.eval()
-                    
-                    preds = self.CDM.model(user_ids, question_ids, category_ids)
-                    sum_acc_0 += utils.micro_ave_accuracy(labels, preds)
-    
-                    meta_preds = self.CDM.model(m_user_ids, m_question_ids, m_category_ids)
+                    with torch.no_grad() :                    
+                        preds = self.CDM.model(user_ids, question_ids, category_ids)
+                        sum_acc_0 += utils.micro_ave_accuracy(labels, preds)
+
+                        meta_preds = self.CDM.model(m_user_ids, m_question_ids, m_category_ids)
     
                     self.CDM.model.train()
-    
                     user_params_optimizer.zero_grad()
-    
+                    
                     with torch.amp.autocast('cuda'):
                         loss = self.CDM._compute_loss(user_ids, question_ids, labels, category_ids)
                         sum_loss_0 += loss.item()
@@ -183,22 +182,20 @@ class AbstractSelectionStrategy(ABC):
                     user_params_scaler.scale(loss).backward()
                     user_params_scaler.step(user_params_optimizer)
                     user_params_scaler.update()
-    
-                    with torch.amp.autocast('cuda'):
+
+                    self.CDM.model.eval()
+                    with torch.no_grad(), torch.amp.autocast('cuda'):
                         loss2 = self.CDM._compute_loss(user_ids, question_ids, labels, category_ids)
                         sum_loss_1 += loss2.item()
     
-                    self.CDM.model.eval()
-    
                     preds = self.CDM.model(user_ids, question_ids, category_ids)
     
-                    self.CDM.model.train()
                     sum_acc_1 += utils.micro_ave_accuracy(labels, preds)
                     sum_meta_acc += utils.micro_ave_accuracy(meta_labels, meta_preds)
                     
-                with torch.amp.autocast('cuda'):
-                    meta_loss = self.CDM._compute_loss(m_user_ids, m_question_ids, meta_labels, m_category_ids)
-                    sum_meta_loss += meta_loss.item()
+                    with torch.no_grad(),torch.amp.autocast('cuda') :
+                        meta_loss = self.CDM._compute_loss(m_user_ids, m_question_ids, meta_labels, m_category_ids)
+                        sum_meta_loss += meta_loss.item()
                     
                 logging.debug(
                     f'inner epoch {t} - query loss_0 : {sum_loss_0/n_batches:.5f} '
@@ -307,6 +304,8 @@ class AbstractSelectionStrategy(ABC):
         
         test_dataset.split_query_meta(self.config['seed'])
 
+        assert self.CDM.initialized_users_prior, \
+            f'Users\' embedding and CDM regularization need to be initialized with the aposteriori distribution.'
         self.CDM.init_test(test_dataset)
 
         test_query_env = QueryEnv(test_dataset, self.device, self.config['valid_batch_size'])
@@ -333,8 +332,9 @@ class AbstractSelectionStrategy(ABC):
                 test_query_env.update(actions, t)
 
                 self.update_users(test_query_env.feed_IMPACT_sub(),(m_user_ids, m_question_ids, m_category_ids),m_labels)
-                    
-                preds = self.CDM.model(m_user_ids, m_question_ids, m_category_ids)
+
+                with torch.no_grad() :
+                    preds = self.CDM.model(m_user_ids, m_question_ids, m_category_ids)
 
                 pred_list[t].append(preds)
                 label_list[t].append(m_labels)
@@ -410,6 +410,9 @@ class AbstractSelectionStrategy(ABC):
         self.CDM_optimizer = torch.optim.Adam(self.CDM.model.parameters(), lr=self.config['inner_lr'])
         self.CDM_scaler = torch.amp.GradScaler(self.config['device'])
 
+        # self.meta_optimizer = torch.optim.Adam(self.CDM.model.parameters(), lr=self.config['inner_lr'])
+        # self.CDM_scaler = torch.amp.GradScaler(self.config['device'])
+
         self.model.train()
         self.CDM.model.train()
 
@@ -472,8 +475,8 @@ class AbstractSelectionStrategy(ABC):
             if (ep + 1) % eval_freq == 0:
                 with torch.no_grad(), torch.amp.autocast('cuda'):
                     valid_loss, valid_metric = self.evaluate_valid(valid_loader, valid_query_env)
-
-                    logging.info(f'{valid_metric}, {valid_loss}')
+                    valid_loss, valid_metric = 8,5
+                    logging.info(f'valid_metric : {valid_metric}, valid_loss : {valid_loss}')
 
                     with warnings.catch_warnings(record=True) as w:
                         warnings.simplefilter("always")
