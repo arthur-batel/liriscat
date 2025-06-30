dca321be21510eb416011f6cded12ad292991e56
Arthur Batel <arthur.batel@insa-lyon.fr>
2025-06-30 10:35:47 +0200

    CDM pretraining script

--- experiments/notebook_example/CDM_pre_train.py
+++ experiments/notebook_example/CDM_pre_train.py
@@ -1,9 +1,8 @@
-from liriscat.utils import generate_eval_config
-import json
-from IMPACT.dataset import LoaderDataset as IMPACT_dataset
 from liriscat.CDM import *
 from IMPACT import model
-from liriscat.dataset.preprocessing_utilities import *
+from IMPACT.utils import generate_eval_config
+from IMPACT.dataset import LoaderDataset as IMPACT_dataset
+from liriscat.dataset import preprocessing_utilities as pu
 import argparse
 
 def main(dataset_name):
@@ -12,22 +11,23 @@ def main(dataset_name):
     # Set all the required parameters ---------------
     IMPACT_config = generate_eval_config(num_epochs=200, save_params=True, dataset_name=dataset_name,
                                          embs_path="../embs/" + dataset_name, params_path="../ckpt/" + dataset_name,
-                                         learning_rate=0.02026, lambda_=1.2e-5, batch_size=2048, valid_metric='rmse',
-                                         pred_metrics=["rmse"])
+                                         learning_rate=0.02026, lambda_=1.2e-5, batch_size=2048,valid_metric='mi_acc', pred_metrics=["mi_acc"],profile_metrics=['doa'])
 
-    concept_map, metadata, nb_modalities = load_dataset_resources(IMPACT_config)
+    concept_map, metadata, nb_modalities = pu.load_dataset_resources(IMPACT_config)
 
-    for i in range(folds_nb):
-        horizontal_train, horizontal_valid = horizontal_data(IMPACT_config, i)
+    for i_fold in range(folds_nb):
 
-        impact_train_data = IMPACT_dataset(horizontal_train, concept_map, metadata, nb_modalities)
-        impact_valid_data = IMPACT_dataset(horizontal_valid, concept_map, metadata, nb_modalities)  # <---
+        IMPACT_config['i_fold'] = i_fold
+        vertical_train, vertical_valid = pu.vertical_data(IMPACT_config, i_fold)
 
-        IMPACT_config['i_fold'] = i
+        impact_train_data = IMPACT_dataset(vertical_train, concept_map, metadata, nb_modalities)
+        impact_valid_data = IMPACT_dataset(vertical_valid, concept_map, metadata, nb_modalities)
+
         algo = model.IMPACT(**IMPACT_config)
         algo.init_model(impact_train_data, impact_valid_data)
         algo.train(impact_train_data, impact_valid_data)
         print(algo.evaluate_predictions(impact_valid_data))
+        print(algo.evaluate_profiles(impact_valid_data))
 
 def parse_args():
     parser = argparse.ArgumentParser(description="A program that runs the CDM pre-training session")
