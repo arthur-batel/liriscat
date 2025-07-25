c5fbacb0e6614690a3fdf6b2f13089eed6b031c0
Arthur Batel <arthur.batel@insa-lyon.fr>
2025-07-25 16:00:07 +0200

    NCDM pretraining

---
+++ liriscat/CDM/NCDM.py
@@ -0,0 +1,263 @@
+import functools
+from collections import defaultdict
+
+from torch.masked import MaskedTensor
+import torch.nn as nn
+from IMPACT.model.IMPACT import resp_to_mod
+
+import torch.utils.data as data
+
+from EduCDM import NCDM
+
+from IMPACT.dataset import *
+from IMPACT.model import IMPACT, IMPACTModel_low_mem, IMPACTModel, custom_loss_low_mem, custom_loss
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from liriscat import dataset
+from liriscat import utils
+
+import warnings
+import torch
+import logging
+
+warnings.filterwarnings(
+    "ignore",
+    message=r".*The PyTorch API of MaskedTensors is in prototype stage and will change in the near future. Please open a Github issue for features requests and see our documentation on the torch.masked module for further information about the project.*",
+    category=UserWarning
+)
+
+
+
+class CATNCDM(NCDM) :
+
+    def __init__(self, **config):
+        self.config=config
+        self.initialized_users_prior = False
+
+        
+    def init_CDM_model(self, train_data: dataset.Dataset, valid_data: dataset.Dataset):
+        super().init_model(train_data.n_categories,train_data.n_questions, train_data.n_users)
+
+        if self.config['load_params']:
+            self.load()
+        # Replacement of pretrained users embeddings with randomly generated ones
+        self.model.train_valid_users = torch.tensor(list(train_data.users_id.union(valid_data.users_id)), device=self.config['device'])
+        
+
+    @property
+    def model(self):
+        return self.ncdm_net
+    
+    def get_params(self):
+        return self.model.state_dict()
+
+    def init_test(self, test_data):
+        pass  
+
+    def init_users_prior(self, train_data, valid_data):
+        """
+        Set the regularization term based on the posterior distribution learned on train and valid users.
+        """
+        train_valid_users = self.model.train_valid_users.to(dtype=torch.long)
+        
+        # NO data leak to test dataset because we only look at the train and valid users 
+        user_embeddings = self.model.users_emb(train_valid_users).float().detach()
+        
+        ave = user_embeddings.mean(dim=0)
+
+        self.model.cov_matrix = torch.cov(user_embeddings.T).to(dtype=torch.float)
+
+        self.model.prior_cov_inv = torch.inverse(self.model.cov_matrix)
+        self.model.prior_mean = ave.unsqueeze(0)
+        
+        self.initialized_users_prior = True
+
+    def set_regularizer_with_prior(self):
+        self.get_regularizer = self.get_regularizer_with_prior
+
+    def get_regularizer_with_prior(self,unique_users, unique_items, user_emb):    
+        A = (user_emb[unique_users] - self.model.prior_mean)  # [nb_users, d_in]
+        S = self.model.prior_cov_inv
+        SA_T = torch.matmul(A, S)  
+
+        return torch.bmm(SA_T.unsqueeze(1), A.unsqueeze(2)).sum()
+
+    def _loss_function(self, users_id, items_id, concepts_id, labels):
+        return self._compute_loss(users_id, items_id, concepts_id, labels)
+
+    def _compute_loss(self, users_id, items_id, concepts_id, labels, learning_users_emb):
+
+        preds = self.forward(users_id, items_id, concepts_id, learning_users_emb)
+
+        L1 = nn.BCELoss(preds,labels)
+
+        unique_users =  torch.unique(users_id)
+        unique_items = torch.unique(items_id)
+
+        R = self.get_regularizer(unique_users, unique_items, learning_users_emb)
+
+        return L1, torch.tensor([1.0]), R
+    
+    def get_regularizer(self,unique_users, unique_items, learning_users_emb):
+
+        return learning_users_emb[unique_users].norm().pow(2)
+    
+    def forward(self, users_id, items_id, concepts_id, users_emb):
+
+        stu_emb = users_emb[users_id]  # [batch_size, knowledge_dim]
+        stat_emb = torch.sigmoid(stu_emb)
+        k_difficulty = torch.sigmoid(self.k_difficulty(items_id))
+        e_difficulty = torch.sigmoid(self.e_difficulty(items_id))  # * 10
+        # prednet
+        input_x = e_difficulty * (stat_emb - k_difficulty) * concepts_id
+        input_x = self.drop_1(torch.sigmoid(self.prednet_full1(input_x)))
+        input_x = self.drop_2(torch.sigmoid(self.prednet_full2(input_x)))
+        output_1 = torch.sigmoid(self.prednet_full3(input_x))
+
+        return output_1.view(-1)
+
+    
+    def get_modalities_emb(self, items_id):
+
+        # Compute item-response indices
+        im_idx = self.model.im_idx[items_id]  # [batch_size, nb_mod]
+        im_emb_prime = self.model.item_response_embeddings(im_idx)  # [batch_size, nb_mod, embedding_dim]
+
+        return im_emb_prime
+
+# coding: utf-8
+# 2021/4/1 @ WangFei
+
+import logging
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import roc_auc_score, accuracy_score
+from EduCDM import CDM
+
+
+class PosLinear(nn.Linear):
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        weight = 2 * F.relu(1 * torch.neg(self.weight)) + self.weight
+        return F.linear(input, weight, self.bias)
+
+
+class Net(nn.Module):
+
+    def __init__(self, knowledge_n, exer_n, student_n):
+        self.knowledge_dim = knowledge_n
+        self.exer_n = exer_n
+        self.emb_num = student_n
+        self.stu_dim = self.knowledge_dim
+        self.prednet_input_len = self.knowledge_dim
+        self.prednet_len1, self.prednet_len2 = 512, 256  # changeable
+
+        super(Net, self).__init__()
+
+        # prediction sub-net
+        self.student_emb = nn.Embedding(self.emb_num, self.stu_dim)
+        self.k_difficulty = nn.Embedding(self.exer_n, self.knowledge_dim)
+        self.e_difficulty = nn.Embedding(self.exer_n, 1)
+        self.prednet_full1 = PosLinear(self.prednet_input_len, self.prednet_len1)
+        self.drop_1 = nn.Dropout(p=0.5)
+        self.prednet_full2 = PosLinear(self.prednet_len1, self.prednet_len2)
+        self.drop_2 = nn.Dropout(p=0.5)
+        self.prednet_full3 = PosLinear(self.prednet_len2, 1)
+
+        # initialize
+        for name, param in self.named_parameters():
+            if 'weight' in name:
+                nn.init.xavier_normal_(param)
+
+    @property
+    def users_emb(self):
+        return self.student_emb
+
+    def forward(self, stu_id, input_exercise, input_knowledge_point):
+
+        # before prednet
+        stu_emb = self.student_emb(stu_id)  # [batch_size, knowledge_dim]
+        stat_emb = torch.sigmoid(stu_emb)
+        k_difficulty = torch.sigmoid(self.k_difficulty(input_exercise))
+        e_difficulty = torch.sigmoid(self.e_difficulty(input_exercise))  # * 10
+        # prednet
+        input_x = e_difficulty * (stat_emb - k_difficulty) * input_knowledge_point
+        input_x = self.drop_1(torch.sigmoid(self.prednet_full1(input_x)))
+        input_x = self.drop_2(torch.sigmoid(self.prednet_full2(input_x)))
+        output_1 = torch.sigmoid(self.prednet_full3(input_x))
+        return output_1.view(-1)
+
+
+class NCDM(CDM):
+    '''Neural Cognitive Diagnosis Model'''
+
+    def __init__(self, knowledge_n, exer_n, student_n):
+        super(NCDM, self).__init__()
+        self.ncdm_net = Net(knowledge_n, exer_n, student_n)
+
+    def train(self, train_data, test_data=None, epoch=10, device="cpu", lr=0.002, silence=False):
+        self.ncdm_net = self.ncdm_net.to(device)
+        self.ncdm_net.train()
+        loss_function = nn.BCELoss()
+        optimizer = optim.Adam(self.ncdm_net.parameters(), lr=lr)
+        for epoch_i in range(epoch):
+            epoch_losses = []
+            batch_count = 0
+            for batch_data in tqdm(train_data, "Epoch %s" % epoch_i):
+                batch_count += 1
+                user_id, item_id, knowledge_emb, y = batch_data
+                user_id: torch.Tensor = user_id.to(device)
+                item_id: torch.Tensor = item_id.to(device)
+                knowledge_emb: torch.Tensor = knowledge_emb.to(device)
+                y: torch.Tensor = y.to(device)
+                pred: torch.Tensor = self.ncdm_net(user_id, item_id, knowledge_emb)
+                loss = loss_function(pred, y)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                epoch_losses.append(loss.mean().item())
+
+            print("[Epoch %d] average loss: %.6f" % (epoch_i, float(np.mean(epoch_losses))))
+
+            if test_data is not None:
+                auc, accuracy = self.eval(test_data, device=device)
+                print("[Epoch %d] auc: %.6f, accuracy: %.6f" % (epoch_i, auc, accuracy))
+
+    def eval(self, test_data, device="cpu"):
+        self.ncdm_net = self.ncdm_net.to(device)
+        self.ncdm_net.eval()
+        y_true, y_pred = [], []
+        for batch_data in tqdm(test_data, "Evaluating"):
+            user_id, item_id, knowledge_emb, y = batch_data
+            user_id: torch.Tensor = user_id.to(device)
+            item_id: torch.Tensor = item_id.to(device)
+            knowledge_emb: torch.Tensor = knowledge_emb.to(device)
+            pred: torch.Tensor = self.ncdm_net(user_id, item_id, knowledge_emb)
+            y_pred.extend(pred.detach().cpu().tolist())
+            y_true.extend(y.tolist())
+
+        return roc_auc_score(y_true, y_pred), accuracy_score(y_true, np.array(y_pred) >= 0.5)
+    
+    def save(self) :
+        path = self.config['params_path'] +  self.config['dataset_name']+'_'+ self.name + '_fold_' + str(self.config['i_fold']) + '_seed_' + str(
+            self.config['seed'])
+        torch.save(self.model.state_dict(), path+".pt")
+
+    def save(self, filepath):
+        torch.save(self.ncdm_net.state_dict(), filepath)
+        logging.info("save parameters to %s" % filepath)
+
+    def load(self, filepath):
+        self.ncdm_net.load_state_dict(torch.load(filepath))  # , map_location=lambda s, loc: s
+        logging.info("load parameters from %s" % filepath)
+
+    def load(self):
+        path = self.config['params_path'] +  self.config['dataset_name']+'_'+ self.name + '_fold_' + str(self.config['i_fold']) + '_seed_' + str(
+            self.config['seed'])
+        self.load(path+".pt")
