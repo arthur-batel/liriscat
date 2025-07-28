165951442319bbe331246b1573765033924e9f97
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-07-28 16:23:41 +0200

    transform deletion

--- liriscat/dataset/preprocessing_utilities.py
+++ liriscat/dataset/preprocessing_utilities.py
@@ -28,19 +28,6 @@ cat_absolute_path = os.path.abspath('../../')
 
 from liriscat import utils as utils_liriscat
 
-def transform(user, item, item2knowledge, score, batch_size, n_dim):
-    knowledge_emb = torch.zeros((len(item), n_dim))
-    for idx in range(len(item)):
-        knowledge_emb[idx][np.array(item2knowledge[item[idx].item()]) - 1] = 1.0
-
-    data_set = TensorDataset(
-        torch.tensor(user, dtype=torch.int64),  # (1, user_n) to (0, user_n-1)
-        torch.tensor(item, dtype=torch.int64),  # (1, item_n) to (0, item_n-1)
-        knowledge_emb,
-        torch.tensor(score, dtype=torch.float32)-1
-    )
-    return DataLoader(data_set, batch_size=batch_size, shuffle=True)
-
 def stat_unique(data: pd.DataFrame, key):
     """
     Calculate statistics on unique values in a DataFrame column or combination of columns.
